a92c305b616ad93eb5ec7432601c8d81c6f936e4
Philippe Ferrara <philippe.ferrara@hotmail.ch>
2024-06-28 01:57:44 +0200

    Section 15.1 : Assignment

---
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
@@ -0,0 +1,28 @@
+package guru.sfg.brewery.security.listeners;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AuthenticationFailureListener {
+
+    @EventListener
+    public void listen(AuthenticationFailureBadCredentialsEvent event) {
+        log.debug("Login failure");
+        if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
+            UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
+            log.debug("Token name : " + token.getPrincipal());
+            if (token.getDetails() instanceof WebAuthenticationDetails) {
+                WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
+                log.debug("Remote address : " + details.getRemoteAddress());
+            }
+
+        }
+    }
+
+}
